32d16a60e45f027c2d30e4922b12eb523e2d620f
mendesg0 <gabriel@prestige.com.br>
2020-10-30 16:48:35 -0300

    feat: added mongoose config

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 import { routes } from "./routes";
+
+dotenv.config();
+
+mongoose.connect(process.env.DATABASE_URI || "", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const app = express();
 app.use(express.json());
